db7f7a2182e66f0172cd10ad26b95a8976d64d24
yashpatel5400 <ypatel@princeton.edu>
2017-04-02 06:50:55 +0000

    Added basic templating for Flask

--- app.py
+++
@@ -1,7 +0,0 @@
-"""
-__authors__     = Yash, Will, Peter
-__description__ = HackPrinceton 2017 file for doing speaker diarization
-using external Ruby hook -- i.e. API of specifying input file and doing
-processing w/ Ruby package for diarization with cleaned outputs
-"""
-
--- app/views.py
+++ app/views.py
@@ -3,12 +3,14 @@ __authors__     = Yash, Will, Peter
 __description__ = Views of pages
 """
 
+from flask import render_template
 from app import app
 
 @app.route('/mic')
 def mic():
     pass
-
+ 
 @app.route('/')
 def index():
-    return("Hello, World!")
+    return render_template('index.html',
+                           title='Home')
