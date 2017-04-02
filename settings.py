f440195e7d137e60e2958ea7262ad21b647b2a96
hypn0z <hypn0z@chaboi.localdomain>
2017-04-02 03:43:25 +0000

    Added diarization scripts

---
+++ settings.py
@@ -0,0 +1,11 @@
+"""
+__authors__     = Yash, Will, Peter
+__description__ = Global variables for the Python files (largely for
+doing organization)
+"""
+
+# ------------------------------ Directory variables -----------------------------
+INPUT_DIR = "audio"
+
+# ------------------------------ Ruby diarizer ---=-----------------------------
+DIARIZER  = "diarize.rb"
