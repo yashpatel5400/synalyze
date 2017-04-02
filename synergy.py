7e1b6c30e3819546cf2f15a5780660cc253c32e3
William Long <wlong0827@gmail.com>
2017-04-01 20:04:25 -0400

    pipeline done

--- synergy.py
+++ synergy.py
@@ -1,6 +1,7 @@
 import requests
 import json
-from watson_developer_cloud import ToneAnalyzerV3, SpeechToTextV1, PersonalityInsightsV3, DiscoveryV1
+from watson_developer_cloud import ToneAnalyzerV3, SpeechToTextV1, \
+									PersonalityInsightsV3, DiscoveryV1
 
 API = {'S2T' : {'KEY':'6e38287f-f949-4389-a5bb-5fff4d30374e', 'PWD':'ZZfb1YQ8k7cl'}, 
 		'TA' : {'KEY':'9453ac25-7842-46de-ab9b-3c117538fdc9', 'PWD':'vrmc1sNpyT4j'},
@@ -8,77 +9,101 @@ API = {'S2T' : {'KEY':'6e38287f-f949-4389-a5bb-5fff4d30374e', 'PWD':'ZZfb1YQ8k7c
 		'CS' : {'KEY':'599eec4e-297c-48cf-b538-7c287cb05ef0', 'PWD':'QXxgp6hpqRDJ'}
 	}
 
-# speech_to_text = SpeechToTextV1(
-#     username = API['S2T']['KEY'],
-#     password = API['S2T']['PWD'],
-#     x_watson_learning_opt_out=False,
-# )
+def transcribe(audio_array):
 
-# with open('./audio.flac', 'rb') as audio_file:
-#     transcript = speech_to_text.recognize(
-#         audio_file, content_type='audio/flac', timestamps=True,
-#         word_confidence=True, continuous=True)
-#     transcript = transcript['results'][0]['alternatives'][0]['transcript']
-#     print transcript
+	speech_to_text = SpeechToTextV1(
+	    username = API['S2T']['KEY'],
+	    password = API['S2T']['PWD'],
+	    x_watson_learning_opt_out=False,
+	)
 
-# tone_analyzer = ToneAnalyzerV3(
-#     username = API['TA']['KEY'],
-#     password = API['TA']['PWD'],
-#     version ='2016-05-19')
+	text_array = []
 
-# with open('./transcript.txt') as tone_text:
-# 	print(json.dumps(tone_analyzer.tone(text=transcript), indent=2))
+	for audio_file in audio_array:
+	    transcript = speech_to_text.recognize(
+	        audio_file, content_type='audio/flac', timestamps=True,
+	        word_confidence=True, continuous=True)
 
-# personality_insights = PersonalityInsightsV3(
-#     version = '2016-10-20',
-#     username = API['PI']['KEY'],
-#     password = API['PI']['PWD'])
+	    results = transcript['results'][0]['alternatives']
+	    transcript = results[0]['transcript']
+	    text_array.append(str(transcript))
 
-# with open('./transcript.txt') as profile_text:
-#     profile = personality_insights.profile(
-#         profile_text.read(), content_type='text/plain',
-#         raw_scores=True, consumption_preferences=True)
+	return text_array
 
-#     print(json.dumps(profile, indent=2))
+def analyze_tone(text_array):
 
-discovery = DiscoveryV1(
-  username = API['CS']['KEY'],
-  password = API['CS']['PWD'],
-  version='2016-12-01'
-)
+	tone_analyzer = ToneAnalyzerV3(
+	    username = API['TA']['KEY'],
+	    password = API['TA']['PWD'],
+	    version ='2016-05-19')
 
-environments = discovery.get_environments()
-# print(json.dumps(environments, indent=2))
+	tone_array = []
 
-news_environments = [x for x in environments['environments'] if
-                     x['name'] == 'byod']
-news_environment_id = news_environments[0]['environment_id']
-# print(json.dumps(news_environment_id, indent=2))
+	for text in text_array:
+		tone_array.append(json.dumps(tone_analyzer.tone(text=text)))
 
-collections = discovery.list_collections(news_environment_id)
-news_collections = [x for x in collections['collections']]
-# print(json.dumps(collections, indent=2))
+	return tone_array
 
-configurations = discovery.list_configurations(
-    environment_id=news_environment_id)
-# print(json.dumps(configurations, indent=2))
+def personalize(text_array):
 
-default_config_id = discovery.get_default_configuration_id(
-    environment_id=news_environment_id)
-# print(json.dumps(default_config_id, indent=2))
+	personality_insights = PersonalityInsightsV3(
+	    version = '2016-10-20',
+	    username = API['PI']['KEY'],
+	    password = API['PI']['PWD'])
 
-default_config = discovery.get_configuration(
-    environment_id=news_environment_id, configuration_id=default_config_id)
-# print(json.dumps(default_config, indent=2))
+	personality_array = []
 
-query_options = {}
-query_results = discovery.query(news_environment_id,
-                                news_collections[1]['collection_id'],
-                                query_options)
-print(json.dumps(query_results, indent=2))
+	for profile_text in text_array:
+	    profile = personality_insights.profile(
+	        profile_text, content_type='text/plain',
+	        raw_scores=True, consumption_preferences=True)
 
-# with open(os.path.join(os.getcwd(),'..','resources','simple.html')) as fileinfo:
-#     res = discovery.add_document(environment_id=writable_environment_id,
-#                                 collection_id=collections['collections'][0]['collection_id'],
-#                                 fileinfo=fileinfo)
-#    print(res)
+	    personality_array.append(json.dumps(profile, indent=2))
+
+	return personality_array
+
+def cognitive_search(query_options):
+
+	discovery = DiscoveryV1(
+	  username = API['CS']['KEY'],
+	  password = API['CS']['PWD'],
+	  version='2016-12-01'
+	)
+
+	environments = discovery.get_environments()
+
+	my_environments = [x for x in environments['environments'] if
+	                     x['name'] == 'byod']
+	my_environment_id = my_environments[0]['environment_id']
+
+	collections = discovery.list_collections(my_environment_id)
+	my_collections = [x for x in collections['collections']]
+
+	configurations = discovery.list_configurations(
+	    environment_id=my_environment_id)
+
+	default_config_id = discovery.get_default_configuration_id(
+	    environment_id=my_environment_id)
+
+	default_config = discovery.get_configuration(
+	    environment_id=my_environment_id, configuration_id=default_config_id)
+
+	query_options = {}
+	query_results = discovery.query(my_environment_id,
+	                                my_collections[1]['collection_id'],
+	                                query_options)
+	
+	return json.dumps(query_results, indent=2)
+
+# ------------- Test Space ------------------
+# a = open('./audio.flac', 'r')
+f = open('./transcript.txt', 'r')
+lines = f.readlines()
+text = ''
+for line in lines:
+	text += line
+# audio_array = [a]
+# text_array = transcribe(audio_array)
+# tone_array = analyze_tone(['several tornadoes touch down as a line of severe thunderstorms swept through Colorado on Sunday '])
+personality_array = personalize([text])
+
