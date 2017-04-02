108601faca560d05e4bd125c3690b33c3468d849
yashpatel5400 <ypatel@princeton.edu>
2017-04-02 08:29:31 +0000

    Updated to include start/stop command

---
+++ app/settings.py
@@ -0,0 +1,8 @@
+"""
+__authors__     = Yash, Will, Peter
+__description__ = Global variables for the Python files (largely for
+doing organization) for app interface
+"""
+
+# ------------------------------  variables -----------------------------
+OUTPUT_DIR = "segment/audio"
--- app/views.py
+++ app/views.py
@@ -9,7 +9,11 @@ from app import app
 @app.route('/mic')
 def mic():
     return render_template('index.html')
- 
+
+@app.route('/stop')
+def stop():
+    return render_template('index.html')
+
 @app.route('/')
 def index():
     return render_template('index.html')
