b42424d1c5076abac2e214eac247726284cac99f
yashpatel5400 <ypatel@princeton.edu>
2017-06-11 01:42:39 +0000

    Slight change for DO deployment

--- wsgi.py
+++ wsgi.py
@@ -4,6 +4,7 @@ __description__ = App used to initialize/run the web backend --
 runs on localhost by default
 """
 
-#!flask/bin/python
 from app import app
-app.run(debug=True)
+
+if __name__ == "__main__":
+    app.run()
