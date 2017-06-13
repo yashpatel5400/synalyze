e441a7c7c5adfa4281d32503554593f2f49db51b
yashpatel5400 <ypatel@princeton.edu>
2017-06-12 23:15:51 -0400

    Cleaned repo. Began adding socket handle

--- app/views.py
+++ app/views.py
@@ -6,6 +6,8 @@ __description__ = Views of pages
 from flask import render_template
 from flask_uwsgi_websocket import GeventWebSocket
 
+import numpy as np
+
 from app import app, mic
 from app.analyze import synergy
 from app.segment.get_speaker import get_speaker
@@ -32,19 +34,10 @@ def index():
 
 @ws.route('/websocket')
 def audio(ws):
-    first_message = True
-    total_msg = ""
-    sample_rate = 0
-
     while True:
        msg = ws.receive()
-
-       if first_message and msg is not None: # the first message should be the sample rate
-          sample_rate = getSampleRate(msg)
-          first_message = False
-          continue
-       elif msg is not None:
-          audio_as_int_array = numpy.frombuffer(msg, 'i2')
-          doSomething(audio_as_int_array)
+       if len(msg) != 0:
+          audio_as_int_array = np.frombuffer(msg, 'i2')
+          print(audio_as_int_array)
        else:
           break
