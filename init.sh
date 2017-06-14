333ae97a8ca990fb4fc1804847b1f2e996a00a18
Ubuntu <ubuntu@ip-172-31-53-142.ec2.internal>
2017-06-14 00:10:25 +0000

    Updated requirements

---
+++ init.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+# shell script for initializing Ubuntu AWS EC2 to run server
+
+sudo apt-get update
+
+sudo apt-get install -y gcc emacs24
+sudo apt-get install -y python-dev python-virtualenv
+sudo apt-get install -y python3-pip python3-dev
+
+sudo apt-get install -y libffi-dev
+sudo apt-get install -y portaudio19-dev
+sudo apt-get install -y libssl-dev
+
+git clone https://github.com/yashpatel5400/synalyze.git
+cd synalyze
+virtualenv synalyze
+source synalyze/bin/activate
+pip install -r requirements.txt
